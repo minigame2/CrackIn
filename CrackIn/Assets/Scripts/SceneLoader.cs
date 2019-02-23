b35bae293060b65f179603cb4dcdef2c1dd218dc
Carolin Hollweger <carolinhollweger@carolins-mbp.home>
2019-02-23 18:43:42 +0100

    update StopWatch, Player, 23.02.19

--- CrackIn/Assets/Scripts/SceneLoader.cs
+++ CrackIn/Assets/Scripts/SceneLoader.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour {
+public class SceneLoader : MonoBehaviour
+{
+    enum Scenes { StartScene, MainScene, GameOverScene };
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene((int)Scenes.StartScene);
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene((int)Scenes.MainScene);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene((int)Scenes.GameOverScene);
+    }
+
 }
--- CrackIn/Assets/Scripts/StopWatch.cs
+++ CrackIn/Assets/Scripts/StopWatch.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StopWatch : MonoBehaviour {
+public class StopWatch : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Text watch;
+    private float timer;
+    private int interval = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        watch = gameObject.GetComponent<Text>();
+        watch.text = "00:00:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        watch.text = string.Format("{0:00}:{1:00}.{2:00}",
+            Mathf.Floor(timer / 60),
+            Mathf.Floor(timer) % 60,
+            Mathf.Floor((timer * 100) % 100));
+        if (timer > interval)
+        {
+            GlobalVariables.speed = GlobalVariables.speed + 1f;
+            interval = interval + 10;
+        }
+    }
 }
